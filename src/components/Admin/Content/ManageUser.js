fbf9f0749349337dd02fad9dd9f696638a90acff
anhtai204 <anhtai22042004@gmail.com>
2024-11-25 10:43:34 +0700

    video 61. hien thi danh sach users

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -2,6 +2,7 @@ import ModalCreateUser from "./ModalCreateUser"
 import './ManageUser.scss'
 import { FcPlus } from 'react-icons/fc'
 import { useState } from "react"
+import TableUser from "./TableUser"
 
 const ManageUser = () => {
 
@@ -19,7 +20,7 @@ const ManageUser = () => {
                     <FcPlus/>Add new user</button>
                 </div>
                 <div className="table-users-container">
-                    table users
+                    <TableUser/>
 
                 </div>
             </div>
